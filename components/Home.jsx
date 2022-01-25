c7834ffb3f02cc5e1e8d58cb0109547167d75c5a
Kirsten tay <taykirsten11@gmail.com>
2022-01-25 11:44:39 +0000

    new project

---
+++ components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View,Text, Image, StyleSheet , ScrollView} from "react-native"
+import {Ionicons ,FontAwesome ,AntDesign,Foundation,FlatList} from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+export default function Home() {
+    return(
+      
+          <view>
+              
+          </view>
+    )
+
+    };
