542ee05d1f73d299410aa4fb9581f704f06bedb8
Kirsten tay <taykirsten11@gmail.com>
2022-02-01 13:55:06 +0000

    started the Login page

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
 import React from 'react'
 import Home from './Screens/Home';
+import Login from './Screens/Login';
 
 
 export default function App() {
+  const MainNavigator = createStackNavigator();
+
+
   return (
-    <View style={styles.container}>
-      <Home/>
-      <StatusBar style="auto" />
+    <View style={{flex:1, }}>
+      <NavigationContainer>
+        <MainNavigator.Navigator
+        screenOptions={{headerShown:false}} 
+        initialRouteName="Home">
+          <MainNavigator.Screen name="Login" component={Login}/>
+          <MainNavigator.Screen name="Home" component={Home}/>
+
+
+
+          </MainNavigator.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
