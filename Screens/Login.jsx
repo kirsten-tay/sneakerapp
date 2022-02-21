3afb7e59bd344dbe8e6a73b77d59c23dcb40eea9
Kirsten tay <taykirsten11@gmail.com>
2022-02-21 14:29:00 +0000

    updated the login page

--- Screens/Login.jsx
+++ Screens/Login.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import {View, Text, ImageBackground} from 'react-native';
+import {View, Text, ImageBackground, TouchableOpacity} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import {AntDesign} from '@expo/vector-icons';
+
 
 
 export default function Login({navigation}) {
     return(
         <View style={{
-            flex:1,
             justifyContent:"center", 
             alignItems:"center", 
             backgroundcolor: "white"
@@ -17,10 +18,29 @@ export default function Login({navigation}) {
                     borderRadius:20, 
                   
                     opacity: 0.70,
-                    transform:[{rotateY:"45deg"}],
-                    
+                    transform:[{rotateY:"45deg"}], 
                   }}>
+                      <View style={{flex:2}}/>
+                      <View style={{flex:2}}>
 
+                   <TouchableOpacity 
+            onPress={() => {
+                navigation.navigate("Cart")
+            }}
+            activeOpacity={0,8}
+            style={{
+                backgroundColor:"#e3e3e3",
+                padding:10,
+                paddingHorizontal:60,
+                flexDirection:"row",
+                alignItems:"center",
+                borderRadius:10,
+                marginTop:20,
+                 }}>
+                     <AntDesign name="google" size={24} color="rgb(256,100,10)"></AntDesign>
+                   <Text style={{fontSize:17, marginLeft:15}}>Login with Gmail</Text>
+                 </TouchableOpacity>
+                 </View>
                   </ImageBackground>
 
             </View>
